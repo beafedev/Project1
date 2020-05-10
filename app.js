f3bb565f037d74e38eebb8e8d2f3c984a7c238e0
billy Chui <bhchu3@hotmail.com>
2020-05-10 18:16:19 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('form');
+const userName = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// function display ERROR message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerHTML = message;
+}
+
+// function display SUCCESS message
+function showSuccess(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = message;
+}
+
+// addEventListener
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (userName.value === '') {
+        showError(userName, 'Username is required');
+    } else {
+        showSuccess(userName, 'form-control success');
+    }
+});
