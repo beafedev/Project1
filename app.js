817587f3c1433ee130caae72795b87caa5b39545
Billy <bhchu3@hotmail.com>
2020-05-18 01:04:31 +1000

    added function to check email is valid

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ function showSuccess(input, message) {
     formControl.className = message;
 }
 
+// function check email is valid
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 // addEventListener
 form.addEventListener('submit', (e) => {
     e.preventDefault();
